c951024f103026f242a16c91d5ad34fa7f5f9296
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-04-02 22:45:52 +0100

    UPDATE : addition of text content for some covid page sections

--- pages/covid.js
+++ pages/covid.js
@@ -3,6 +3,7 @@ import PublicLayout from '@components/public-layout'
 import StatsCard from '@components/stats-card'
 import '@scss/pages/covid.scss'
 import CONFIG from '../config'
+import ReactMarkdown from 'react-markdown'
 
 const statisticsContent = () => {
   return (
@@ -41,6 +42,56 @@ const overviewContent = () => (
   </div>
 )
 
+const symptomsContent = () => (
+  <ReactMarkdown source={`People may be sick with the virus for 1 to 14 days before developing symptoms.
+
+The most common symptoms of coronavirus disease (COVID-19) are **fever, tiredness, and dry cough.**
+
+Most people (about 80%) recover from the disease without needing special treatment.
+
+More rarely, the disease can be serious and even fatal. Older people, and people with other medical conditions (such as asthma, diabetes, or heart disease), may be more vulnerable to becoming severely ill.
+
+**People may experience:**
+- cough
+- fever
+- tiredness
+- difficulty breathing (in severe cases)`} />
+)
+
+const preventionContent = () => (
+  <ReactMarkdown source={`There’s currently no vaccine to prevent coronavirus disease (COVID-19).
+
+You can protect yourself and help prevent spreading the virus to others if you:
+
+**Do**
+
+- Wash your hands regularly for 20 seconds, with soap and water or alcohol-based hand rub
+- Cover your nose and mouth with a disposable tissue or flexed elbow when you cough or sneeze
+- Avoid close contact (1 meter or 3 feet) with people who are unwell
+- Stay home and self-isolate from others in the household if you feel unwell
+
+**Don't**
+
+Touch your eyes, nose, or mouth if your hands are not clean`} />
+)
+
+const treatmentsContent = () => (
+  <ReactMarkdown source={`There is no specific medicine to prevent or treat coronavirus disease (COVID-19). People may need supportive care to help them breathe.
+
+**Self care**
+
+If you have mild symptoms, stay at home until you’ve recovered. You can relieve your symptoms if you:
+
+- rest and sleep
+- keep warm
+- drink plenty of liquids
+- use a room humidifier or take a hot shower to help ease a sore throat and cough
+
+**Medical treatments**
+
+If you develop a fever, cough, and have difficulty breathing, promptly seek medical care. Call in advance and tell your health provider of any recent travel or recent contact with travelers.`} />
+)
+
 const contentSwitch = (sectionHash) => {
   switch(sectionHash) {
     case "statistics":
@@ -50,13 +101,13 @@ const contentSwitch = (sectionHash) => {
       return overviewContent()
       break
     case "symptoms":
-      return null
+      return symptomsContent()
       break
     case "prevention":
-      return null
+      return preventionContent()
       break
     case "treatments":
-      return null
+      return treatmentsContent()
       break
     case "faqs":
       return null
