88054942f5f4d1fb71c0402acde360571dc0a828
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-04-06 16:03:44 +0100

    UPDATE : async defer botsupply chatbot script

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -7,7 +7,7 @@ const ChatbotPage = () => {
       <section style={sectionStyle}>
         <h1>Chat with our Chatbot</h1>
       </section>
-      {/* <script src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript"></script> */}
+      {/* <script async defer src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript"></script> */}
     </PublicLayout>
   )
 }
@@ -16,6 +16,7 @@ const sectionStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  textAlign: 'center',
   minHeight: '80vh',
 }
 
