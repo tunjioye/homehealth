b8d0a5278a9fc32f7ab02905c48c0ad8c8ea1d88
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-04-13 12:00:21 +0100

    UPDATE : back to default web integration

--- src/components/bot-script.js
+++ src/components/bot-script.js
@@ -1,16 +1,7 @@
-import React from 'react'
-
-class BotScript extends React.Component {
-  componentDidMount () {
-    if (window['Zone']) {
-      window['Zone'] = undefined
-    }
-  }
-
-  render () {
-    return (<script src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript" async></script>)
-    // return (<script type="text/javascript" src="/js/freshchat.min.js" async></script>)
-  }
+const BotScript = () => {
+  return (
+    <script src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript" async></script>
+  )
 }
 
 export default BotScript
