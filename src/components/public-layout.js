3f637c314ff3286565cf52f4718618276636d72c
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-04-13 10:13:27 +0100

    UPDATE : enabled botsupply chatbot script

--- src/components/public-layout.js
+++ src/components/public-layout.js
@@ -42,7 +42,7 @@ class PublicLayout extends React.Component {
         <main className="main-content">
           {children}
           {/* <script type="text/javascript" src="/js/freshchat.min.js"></script> */}
-          {/* <script src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript"></script> */}
+          <script src="https://webchat2.botsupply.ai/sdk/RTOMP4Foy" type="text/javascript"></script>
         </main>
         <PublicFooter />
       </div>
